630006ae99ff5e1d3126dc4c0afcbd41e0594f41
Dhanish18 <dhanishmarudhu@gmail.com>
2023-11-20 22:30:01 +0530

    performed create and delete

--- react-loan-management/src/components/create/create.js
+++ react-loan-management/src/components/create/create.js
@@ -1,55 +1,119 @@
-import Nav from '../nav/nav'
-import './create.css'
-import { useState,useRef } from 'react';
+import Nav from "../nav/nav";
+import "./create.css";
+import { useState, useRef } from "react";
+import axios from "axios";
 
 const Create = () => {
-    const [createForm,setCreateForm] = useState({
-        name:'',
-        phone:'',
-        address:'',
-        provideDate:'',
-        dueMonth:'',
-        amount:'',
-        interest:''
-    })
-    const [error,setError] = useState('')
+  const initialCreateForm = {
+    name: "",
+    phone: "",
+    address: "",
+    provideDate: "",
+    dueMonth: "",
+    amount: "",
+    interest: "",
+  };
+  const [createForm, setCreateForm] = useState(initialCreateForm);
+  const [error, setError] = useState("");
 
-    const handleChange = (e) =>{
-        const {name,value} = e.target;
-        setCreateForm({...createForm,[name]:value})
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCreateForm({ ...createForm, [name]: value });
+  };
+  const handleReset = () => {
+    setCreateForm(initialCreateForm);
+  };
 
-    const handleSave=()=>{
-    if(!createForm.name &&! createForm.phone &&! createForm.address &&! createForm.provideDate &&! createForm.dueMonth &&! createForm.amount &&! createForm.interest){
-        setError('please fill all fields')
-    }else if(createForm.phone.length !== 10){
-        setError('invalid phone number')
-    }else if(createForm.dueMonth < 1 || createForm.amount < 1 || createForm.interest < 1){
-        setError('invalid numbers')
+  const handleSave = async () => {
+    if (
+      !createForm.name &&
+      !createForm.phone &&
+      !createForm.address &&
+      !createForm.provideDate &&
+      !createForm.dueMonth &&
+      !createForm.amount &&
+      !createForm.interest
+    ) {
+      setError("please fill all fields");
+    } else if (createForm.phone.length !== 10) {
+      setError("invalid phone number");
+    } else if (
+      createForm.dueMonth < 1 ||
+      createForm.amount < 1 ||
+      createForm.interest < 1
+    ) {
+      setError("invalid numbers");
+    } else {
+      const response = await axios.post(
+        "http://localhost:3001/items",
+        createForm
+      );
+      console.log("saved", response.data);
+      handleReset();
     }
-    else{
-        console.log(createForm)
-    }
-    } 
+  };
 
-    return(<div className='create'>
-    <Nav />
-    <div className='createContainer'>
-    <h1>create form</h1>
-    <input type='text' placeholder='name' name='name' value={createForm.name} onChange={handleChange}/>
-    <input type='number' placeholder='phone' name='phone' value={createForm.phone} onChange={handleChange}/>
-    <input type='text' placeholder='address' name='address' value={createForm.address} onChange={handleChange}/>
-    <input type='text' placeholder='provide date' name='provideDate' value={createForm.provideDate} onChange={handleChange} />
-    <input type='number' placeholder='due month' name='dueMonth' value={createForm.dueMonth} onChange={handleChange}/>
-    <input type='number' placeholder='amount' name='amount' value={createForm.amount} onChange={handleChange}/>
-    <input type='number' placeholder='interest' name='interest' value={createForm.interest} onChange={handleChange} />
-    <p>{error}</p>
-    <div className='buttonContainer'>
-        <button onClick={handleSave}>save</button>
-        <button >reset</button>
+  return (
+    <div className="create">
+      <Nav />
+      <div className="createContainer">
+        <h1>create form</h1>
+        <input
+          type="text"
+          placeholder="name"
+          name="name"
+          value={createForm.name}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="phone"
+          name="phone"
+          value={createForm.phone}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="address"
+          name="address"
+          value={createForm.address}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="provide date"
+          name="provideDate"
+          value={createForm.provideDate}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="due month"
+          name="dueMonth"
+          value={createForm.dueMonth}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="amount"
+          name="amount"
+          value={createForm.amount}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="interest"
+          name="interest"
+          value={createForm.interest}
+          onChange={handleChange}
+        />
+        <p>{error}</p>
+        <div className="buttonContainer">
+          <button onClick={handleSave}>save</button>
+          <button onClick={handleReset}>reset</button>
+        </div>
+      </div>
     </div>
-    
-    </div>
-    </div>)
-}
-export default Create
+  );
+};
+export default Create;
--- react-loan-management/src/components/read/read.js
+++ react-loan-management/src/components/read/read.js
@@ -1,14 +1,65 @@
-import React from 'react'
-import Nav from '../nav/nav'
-import './read.css'
+import React, { useEffect, useState } from "react";
+import Nav from "../nav/nav";
+import "./read.css";
+import axios from "axios";
 
 const Read = () => {
-  return (
-    <div className='read'>
-    <Nav />
-    <div className='readContainer'><h1>read</h1></div>
-     </div>
-  )
-}
+  const [dataList, setDataList] = useState([]);
 
-export default Read
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/items");
+      setDataList(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3001/items/${id}`);
+      console.log("item deleted");
+      fetchData();
+    } catch (error) {
+      console.error("Error deleting item:", error);
+    }
+  };
+
+  return (
+    <div className="read">
+      <Nav />
+      <div className="readContainer">
+        <h1>read</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Phone Number</th>
+              <th>Address</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataList.map((item) => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.phone}</td>
+                <td>{item.address}</td>
+                <td>
+                  <button>update</button>
+                  <button onClick={() => handleDelete(item.id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Read;
